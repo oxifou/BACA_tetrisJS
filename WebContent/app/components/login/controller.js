11752ffdc68ebf52af62737c40ad2bb914e2a4ab
camille <tal.adoum@gmail.com>
2017-06-13 16:41:16 +0200

    loginjs

--- WebContent/app/components/login/controller.js
+++ WebContent/app/components/login/controller.js
@@ -1,9 +1,23 @@
 var app = angular.module("BACA_tetrisAngularJS");
 
-app.controller("loginController", function($scope, Page) {
+app.controller("loginController", function($scope, Page, utilisateurResource) {
 	
 	Page.setTitle("Login");
+	$scope.connexion = function(){
+		var user = {
+				username : $scope.username,
+				password : $scope.password
+		}
+		utilisateurResource.co(user)
+	};
+	
 	
 
+});
+
+app.factory("utilisateurResource", function(API_URL, $resource) {
+    return $resource(API_URL + '/account/auth', null, {
+        'co': { method: 'POST' }
+    });
 });
 
