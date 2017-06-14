ff002b3e483b5f757b529b0be4cb5c343d06f756
Unknown <malbert.benoit@gmail.com>
2017-06-14 10:54:20 +0200

    login

--- WebContent/app/components/home/controller.js
+++ WebContent/app/components/home/controller.js
@@ -4,5 +4,5 @@ var app = angular.module("BACA_tetrisAngularJS");
 app.controller("homeController", function($scope, Page, userF) {
 	Page.setTitle("Accueil");
 	
-	$scope.username = userF.current.prenom;
+	$scope.prenom = userF.current.prenom;
 });
