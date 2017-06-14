3e4700e5b90aad59c4550f071c5f15fb75762815
Unknown <alexis.reignoux@gmail.com>
2017-06-14 11:34:32 +0200

    changement de paramètrage de partie
    permet de lancer une partie correctement

--- WebContent/app/components/tetris/tetris-provider.js
+++ WebContent/app/components/tetris/tetris-provider.js
@@ -3,9 +3,9 @@ var app = angular.module("BACA_tetrisAngularJS");
 
 
 app.provider("tetris", function() {
-	var largeur = 0;
-	var hauteur = 0;
-	var sizeOfTetriminos = 0;
+	var largeur = 10;
+	var hauteur = 20;
+	var sizeOfTetriminos = 25;
 
 
 	this.setLargeur = function(_largeur) {
