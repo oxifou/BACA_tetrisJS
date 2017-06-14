2cd6ae0a687254e2bdabe055a71a512793304616
camille <tal.adoum@gmail.com>
2017-06-14 14:25:26 +0200

    Score affichage

--- WebContent/app/components/score/controller.js
+++ WebContent/app/components/score/controller.js
@@ -1,7 +1,28 @@
 var app = angular.module("BACA_tetrisAngularJS");
 
-app.controller("scoreController", function($scope, Page, userF) {
+app.controller("scoreController", function($scope, Page, userF, scoreResource) {
 	Page.setTitle("Score");
 	
-	$scope.id = userF.current.id;
+	//$scope.id = userF.current.id;
+	
+	
+		$scope.username = "";
+		$scope.points = 0;
+		$scope.niveau = 0;
+	    $scope.date = "";
+	    $scope.score = scoreResource.query();
+	
+	    
+	    $scope.addScore = function() {
+	    	this.score.push(userF.create(this.username,this.points, this.niveau, this.date));
+	    	
+	    	this.username = "";
+	    	this.points = 0;
+	    	this.niveau = 0;
+	    	this.date = "";
+	    	
+	    };
+	  $scope.show =   function() {
+		  return(this.username != "" && this.points != 0 && this.niveau != 0 && this.date  != "");
+	  }
 });
